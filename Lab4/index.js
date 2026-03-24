349249cea8c2cff9bca0fb8c52c40504a64cad2d
berintanemanuel <natiberintan3@gmail.com>
2026-03-24 15:53:19 +0200

    Change from manual concatenation to each() method

--- Lab4/index.js
+++ Lab4/index.js
@@ -13,10 +13,16 @@ $("#clickMeBtn").on("click", function(event){
 
 $(".submitButton").on('click', function(){
   let computedString = "";
-  computedString += $(".clubInput").val();
-  computedString += $(".nationalityInput").val();
-  computedString += $(".positionInput").val();
-  computedString += $(".fullNameInput").val();
+  //computedString += $(".clubInput").val();
+  //computedString += $(".nationalityInput").val();
+  //computedString += $(".positionInput").val();
+  //computedString += $(".fullNameInput").val();
+
+  classes = [".clubInput", ".nationalityInput", ".positionInput", ".fullNameInput"];
+  $.each(classes, function(idx, name){
+    //console.log(name);
+    computedString += $(name).val();
+  });
 
   let newText = $("#firstName").val() + computedString;
   $("#firstName").val(newText);
