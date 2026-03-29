c49d111bb56289fa8564b8baef9fa4ad246dc5ec
berintanemanuel <natiberintan3@gmail.com>
2026-03-29 15:39:45 +0300

    Implement basic ui for add author page. Submit does not work yet

--- Lab5/add_author.php
+++ Lab5/add_author.php
@@ -3,9 +3,26 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Add author</title>
+  <link rel="stylesheet" href="./styles.css">
 </head>
 <body>
-  Hello World!
+  <div class="container">
+    <h1>Add an author</h1>
+    <form method="post" id="add-author-form">
+      <div class="form-container">
+        <label for="name">Name</label><br>
+        <input type="text" name="name" id="name" required>
+      </div>
+      <div class="form-container">
+        <label for="email">Email</label><br>
+        <input type="text" name="email" id="email" required>
+      </div>
+      <div class="form-submit-container">
+        <a href="./index.php" class="cancel-add-hotel">Cancel</a>
+        <button type="submit">Save</button>
+      </div>
+    </form>
+  </div>
 </body>
 </html>
