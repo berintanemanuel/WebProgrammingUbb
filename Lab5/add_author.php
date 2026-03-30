1c8c80d36aed32e49989249508e3dc2f0b49790d
berintanemanuel <natiberintan3@gmail.com>
2026-03-30 15:06:47 +0300

    Implement Add author feature

--- Lab5/add_author.php
+++ Lab5/add_author.php
@@ -1,3 +1,33 @@
+<?php
+require_once "db.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+
+    try {
+        $database = new Database();
+        $conn = $database->getConnection();
+
+        $query = "INSERT INTO authors (name, email) VALUES (:name, :email)";
+        $stmt = $conn->prepare($query);
+
+        $stmt->bindParam(":name", $name);
+        $stmt->bindParam(":email", $email);
+
+        if ($stmt->execute()) {
+            // Redirect after success
+            header("Location: index.php");
+            exit();
+        } else {
+            echo "Error inserting author.";
+        }
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -20,7 +50,7 @@
       </div>
       <div class="form-submit-container">
         <a href="./index.php" class="cancel-add-hotel">Cancel</a>
-        <button type="submit">Save</button>
+        <button type="submit" id="submit-add-hotel-button">Save</button>
       </div>
     </form>
   </div>
