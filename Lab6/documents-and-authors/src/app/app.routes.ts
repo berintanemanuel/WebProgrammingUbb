d94323a1d0791cddbd67a9c2a5ddbe279a8c35ae
berintanemanuel <natiberintan3@gmail.com>
2026-04-16 22:40:25 +0300

    Create add document component

--- Lab6/documents-and-authors/src/app/app.routes.ts
+++ Lab6/documents-and-authors/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { AddAuthorComponent } from './components/add-author/add-author';
 import { HomeComponent } from './components/home-component/home-component';
 import { ListAuthorsComponent } from './components/list-authors-component/list-authors-component';
+import { AddDocumentComponent } from './components/add-document-component/add-document-component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   {path: 'add-author', component: AddAuthorComponent},
-  {path: 'list-authors-component', component: ListAuthorsComponent}
+  {path: 'list-authors-component', component: ListAuthorsComponent},
+  {path: 'add-document-component', component: AddDocumentComponent}
 ];
---
+++ Lab6/documents-and-authors/src/app/components/add-document-component/add-document-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-document-component',
+  imports: [],
+  templateUrl: './add-document-component.html',
+  styleUrl: './add-document-component.css',
+})
+export class AddDocumentComponent {}
